36b880bb3e529c08756021350c7468f55e514e9f
1010q <kd1370141@st.kobedenshi.ac.jp>
2024-11-12 01:06:31 +0900

    ポート許可、ログイン操作

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'mysecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -16,6 +18,9 @@ migrate = Migrate(app, db)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+@login_manager.unauthorized_handler
+def unauthorized():
+    abort(401)
 
 from models import User, Project
 from views import *
