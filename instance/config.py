9bea16ca40aa3f4d227c0e9b5f7992d746c65cc3
1010q <kd1370141@st.kobedenshi.ac.jp>
2024-11-14 22:29:39 +0900

    デプロイ用

---
+++ instance/config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'miso893-shiru777-wakame3'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///instance/app.db'
+    UPLOAD_FOLDER = 'uploads'
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024
