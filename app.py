6f15f0c36a73e17b43e338a510fb2f94293a0278
1010q <kd1370141@st.kobedenshi.ac.jp>
2024-11-15 18:44:03 +0900

    トークン認証の修正

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+#app.config.from_pyfile('/opt/render/project/src/instance/config.py')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from flaskr.models import *
+from flaskr.views import *
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
